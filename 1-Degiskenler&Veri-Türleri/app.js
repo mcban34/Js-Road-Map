d647368f9f090d4006dcd118b5f1d0d26225bd8a
mehmet çoban <74604824+mcban34@users.noreply.github.com>
2023-06-15 17:35:44 +0300

    değişkenler ve veri türleri

--- 1-Degiskenler&Veri-Türleri/app.js
+++ 1-Degiskenler&Veri-Türleri/app.js
@@ -40,9 +40,65 @@ console.log(dogumYili,id,havaDerece)
 let renkler = ["sarı","beyaz","siyah","yeşil","kırmızı"]
 console.log(renkler)
 
+//*Dizi içerisinde veri çekmek (dizi içerisinde elemanlar index numarasına göre verilir)
+console.log(renkler[0]) //sarı
+console.log(renkler[3]) //yeşil
+
 //*length dizi uzunluğunu bize verir (eleman sayısı diyebiliriz)
 let renklerLenght = renkler.length
 console.log(renkler)
 
+//*dizideki son elemanı almak için (dizi uzunluğundan -1 çıkartılır)
+console.log(renkler[renkler.length-1])
 
+//?obejeler (key-value değerine göre veriler alır (veri tabanı yapısında diyebiliriz))
+let kullanici = {
+    isim:"mehmet",
+    soyisim:"coban",
+    yas:23
+}
+console.log(kullanici)
 
+//*kullanıcı içerisinde keye ulaşmak
+console.log(kullanici.isim)
+console.log(kullanici.soyisim)
+
+//?obje ve dizilerle toplu veriler oluşturmak
+let urunler = [
+    {
+        title:"urun1",
+        content:"urun1 içeriği yazılır",
+        price:25
+    },
+    {
+        title:"urun2",
+        content:"urun2 içeriği yazılır",
+        price:50
+    },
+    {
+        title:"urun3",
+        content:"urun3 içeriği yazılır",
+        price:75
+    },
+]
+
+console.log(urunler) //*[{},{},{}] => dizi içerisindeki toplu objeler tutulur
+
+//*veriye ulaşmak (0. indexdeki content verisini çeker)
+console.log(urunler[0].content) 
+
+//?boolean
+//*boolean bizlere true-false  döndürür
+let veriDurum = true
+let veriDurum2 = false
+console.log(veriDurum,veriDurum2)
+
+//?undefined
+//*undefined tanımsız bir değer anlamına gelir (değişken çekilirken tanımsız bir ifade değerine denk gelirse undefined döndürür)
+let tanimSizDeger = undefined
+console.log(tanimSizDeger)
+
+//?null
+//*null boş bir değer anlamına gelir (çekilen değişkenin içerisinde veri yoksa null değeri verilir)
+let bosDeger = null
+console.log(bosDeger)
