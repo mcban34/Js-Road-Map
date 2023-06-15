363e8af2154f90d924cd354422f8e7b49884c28d
mehmet çoban <74604824+mcban34@users.noreply.github.com>
2023-06-15 16:30:48 +0300

    day1&day2 begginig

---
+++ 2- Veri Yazdırma Yöntemleri/app.js
@@ -0,0 +1,52 @@
+// Veri Yazdırma Yöntemleri
+
+
+/* console.log örnek ve tanımlar*/ 
+
+console.log("Merhaba, Dünya!");  //  JavaScript kodunun çalıştığı ortamın konsoluna mesaj yazdırmak için kullanılır. 
+
+
+let name = 'Emre'
+
+console.log(name) //  Emre
+
+let surname ='Sarigul'
+
+console.log(surname) //Sarigul
+
+let age = 28;
+
+console.log(age)
+
+console.log('Harika ilerliyoruz.')
+
+/* Alert örnek ve tanımlar */
+
+
+alert('JavaScript Öğreniyorum') // bir tarayıcı penceresinde bir uyarı kutusu görüntülemek için kullanılır. 
+
+let message="JavaScript'i seviyorum.";
+
+alert(message); // JavaScript'i seviyorum.
+
+let  users ={
+     name:'Emre',
+     surname : 'Sarıgül',
+     age :28
+};
+
+console.log(users.name) // Emre
+console.log(users.surname)
+
+/* document.write örnek ve tanımlar */
+
+
+document.write('Hello,World') // JavaScript kodunun çalıştığı web sayfasının içeriğine doğrudan yazı eklemek için kullanılır
+
+let country ='Türkiye';
+
+document.write(country) // Türkiye
+
+let city = 'İstanbul';
+
+document.write(city) // İstanbul
