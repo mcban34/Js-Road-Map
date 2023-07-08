fc3019dd73534cb56146f1394f6c6fc84f91d20b
Selcukibar <selcukibarr@icloud.com>
2023-07-08 20:00:47 +0300

    karar vericiler ve karsilastirma operatorleri

---
+++ Ekstralar/Koşullar/app.js
@@ -0,0 +1,29 @@
+//*Mantıksal Karar Vericiler => Doğru ile yanlış (true/false) kavramlarını ayırt edebilen yapılardır.
+
+//?Örnek:
+
+// Soru                          Mantıksal cevap
+// İnsanlar Uçabilir mi?         Yanlış(false)
+// 5, 2'den büyük mü?            Doğru(true)   
+
+//*Karşılaştırma Operatörleri => Değerlerin kıyaslamalarını yapan mantıksal karar vericilerdir.
+
+//*Eşit Değilse (!=)
+//?console.log(4!=1); //true
+
+//*Eşitse(==)
+// console.log(1==2); //false 
+
+//*Büyükse(>)
+// console.log(10>5); //true
+
+//*Küçükse(<)
+// console.log(10<5); //false
+
+//*Büyük veya eşitse(>=)
+// console.log(4>=4); //true
+
+//*Küçük veya eşitse(<=)
+// console.log(4<=8); //false
+
+
