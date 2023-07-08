4ebcc2b7d94dbdce7c7512fb2d76a57a5c4db356
Selcukibar <selcukibarr@icloud.com>
2023-07-08 20:44:21 +0300

    if-else ve else-if

--- Ekstralar/Koşullar/app.js
+++ Ekstralar/Koşullar/app.js
@@ -32,5 +32,43 @@
 //Veya(||)
 // console.log(5==4 || 9<1); //false
 
+//* IF,ElSE(Yukarıdaki Konu başlıklarında (06.dosya if-else) detaylı bir şekilde anlatılmıştır.) =>
+//*Bir şartın doğru veya yanlış olmasına göre belli bir kod bloğunun çalışmasını veya atlaması için kullanılır.
+//*if(eğer) anlamında else(yoksa) anlamına geliyor.
 
+// if(şart){
+//     işlenecek kod burada barındırılır
+// }
+// else{
+// }
+
+// let yas = 16
+// if(yas>18){
+//     console.log("Hoşgeldiniz");
+// }
+// else{
+//     console.log("Yaşınız tutmuyor!");
+// }
+
+//*Not:if koşulunun içine tekrar bir if koşulu açabiliceğiniz gibi else koşulunun içinede if koşulu açabilirsiniz.
+
+//ELSE-IF => if koşulunun olmadığı durumlar için alternatif üretir ve zincirleme olarak çalışır.
+
+//?Dışarıdan hangi ayda olduğunuzu tanımlayın
+//?Hangi ayda olduğumuzu bulunuz.
+
+// let ay = "haziran"
+// if(ay=="haziran"){
+
+//     console.log("yaz mevsimindesiniz");
+// }
+// else if(ay=="ocak"){
+//     console.log("Kış mevsimindesiniz");
+// }
+// else if (ay=="eylül"){
+//     console.log("Sonbahar mevsimindesiniz");
+// }
+// else{
+//     console.log("hatalı giriş");
+// }
 
